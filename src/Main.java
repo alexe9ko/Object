b28fa40e4dd7fc07858a0b21c10fe663fa0b1695
alexe <alexe9ko9@gmail.com>
2025-02-28 08:13:35 +0300

    object

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Autor nikolaiGogol = new Autor("Николай", "Гоголь");
+        Autor fetAfonasiy= new Autor("Фет", " Афонасий");
+        Book viy = new Book( " Вий ", 1833, nikolaiGogol);
+        Book vecher = new Book("Вечер",1855, fetAfonasiy);
+
+        viy.setPublicationYear(1834);
+
+        System.out.println(viy.getName());
+        System.out.println(viy.getPublicationYear());
+
     }
 }
